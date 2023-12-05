d30a3d2fabc72ebac29367ba4598e8d535033c42
Ana <Ana@example.com>
2023-12-05 12:47:52 -0300

    funcionalidades

--- magym/src/app/components/aluga-armario-oficina-mecanica/aluga-armario-oficina-mecanica.component.ts
+++ magym/src/app/components/aluga-armario-oficina-mecanica/aluga-armario-oficina-mecanica.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./aluga-armario-oficina-mecanica.component.css']
 })
 export class AlugaArmarioOficinaMecanicaComponent {
+  onArmarioClick(event: any) {
+    const isOcupado = event.target.classList.contains('ocupado');
 
+    if (!isOcupado) {
+      console.log('Clicou em um item da lista!');
+      // Adicione a lógica desejada aqui
+    } else {
+      console.log('Item ocupado, não pode ser acessado.');
+    }
+  }
 }
